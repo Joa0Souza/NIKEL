642903e9b612b00c799f07bb515e071d2245f686
Joa0Souza <161522615+Joa0Souza@users.noreply.github.com>
2024-02-27 18:05:48 -0300

    index.js

--- NIKEL/public/js/index.js
+++ NIKEL/public/js/index.js
@@ -1,4 +1,4 @@
-const myModal=new bootstrap.Modal("#login-modal")
+const myModal=new bootstrap.Modal("#login-Modal")
 let logged= sessionStorage.getItem("logged");
 const session= localStorage.getItem("session");
 
